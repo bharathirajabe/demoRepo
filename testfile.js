2e5935bf64ad6c3223580d71935c3052c07aeb25
bharathirajabe <karthiksinduja@gmail.com>
2015-10-06 17:17:30 +0530

    testing the git first time

---
+++ testfile.js
@@ -0,0 +1,41 @@
+var app = angular.module('plunker', ['ui.router']);
+
+app.controller('MainCtrl', function($scope, $state) {
+  
+  $scope.callfn = function(){
+  	$state.go('about');
+  }
+})
+.config(function ($stateProvider, $urlRouterProvider) {
+     $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        
+        .state('home', {
+            url: '/home',
+            views:{
+              "viewOne":{
+                template: '<div>hai</div>'
+              },
+              "viewTwo":{
+                template: '<div>how</div>'
+              }
+            }
+            
+        })
+        
+        .state('about', {
+            url: '/about',
+            views:{
+              "viewOne":{
+                template: '<div>2</div>'
+              },
+              "viewTwo":{
+                template: '<div>1</div>'
+              }
+            }
+            
+        })
+       
+  });
