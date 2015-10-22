52882670a77ffb1f5ad873aa92a028d30d555c3a
bharathirajabe <karthiksinduja@gmail.com>
2015-10-22 10:35:43 +0530

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -1,21 +1,20 @@
-var app = angular.module('plunker', ['ui.router']);
-
-app.controller('MainCtrl', function($scope, $state) {
-  
-  $scope.callfn = function(){
-  	$state.go('about');
-  }
-})
-.config(function ($stateProvider, $urlRouterProvider) {
-     $urlRouterProvider.otherwise('/home');
-    
-    $stateProvider
-        
-      I edited locally
-        
-
-       
-  });
+$scope.addInputValidation = function(e){
+				var validRegex = /^[A-Za-z0-9]+$/;
+                var valueInAdd = $(e.currentTarget).val();
+				var enteredChar = String.fromCharCode(e.which);
+                var charCode = (typeof e.which === "number") ? e.which : e.keyCode, chr = String.fromCharCode(charCode);
+                var charChecker = ((isNaN(parseInt(chr)))||(!(enteredChar.match(/^[A-Za-z]+$/))));
+                if((valueInAdd.length > 3 || e.which === 13 || e.which === 9) && !(e.which === 8)){
+					e.preventDefault();
+					return;
+				} else if(!validRegex.test(enteredChar) && !(e.which === 8)){
+					e.preventDefault();
+					return;
+				}
+                if (!charChecker){
+                e.preventDefault();}
+			};
+			/////////////////////////
   
   ///
   
