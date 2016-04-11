26910cfb32d5575afdf36597e9bc34c40320612a
bharathirajabe <karthiksinduja@gmail.com>
2016-04-11 17:24:26 +0530

    Update testfile.js

--- testfile.js
+++ testfile.js
@@ -1,40 +1,253 @@
-$scope.addInputValidation = function(e){
-				var validRegex = /^[A-Za-z0-9]+$/;
-                var valueInAdd = $(e.currentTarget).val();
-				var enteredChar = String.fromCharCode(e.which);
-                var charCode = (typeof e.which === "number") ? e.which : e.keyCode, chr = String.fromCharCode(charCode);
-                var charChecker = ((isNaN(parseInt(chr)))||(!(enteredChar.match(/^[A-Za-z]+$/))));
-                if((valueInAdd.length > 3 || e.which === 13 || e.which === 9) && !(e.which === 8)){
-					e.preventDefault();
-					return;
-				} else if(!validRegex.test(enteredChar) && !(e.which === 8)){
-					e.preventDefault();
-					return;
-				}
-                if (!charChecker){
-                e.preventDefault();}
-			};
-			/////////////////////////
-  
-  ///
-  
-  $scope.addInputValidation = function(e){
-				var validRegex = /^[A-Za-z0-9]+$/;
-				var valueInAdd = $(e.currentTarget).val();
-				var enteredChar = String.fromCharCode(e.which);
-				if((valueInAdd.length > 3 || e.which === 13 || e.which === 9) && !(e.which === 8)){
-					e.preventDefault();
-					return;
-				} else if(!validRegex.test(enteredChar) && !(e.which === 8)){
-					e.preventDefault();
-					return;
-				}
-			};
+Hi,
 
-bitbucket.org
-https://www.atlassian.com/git/tutorials/ 
-https://www.codeschool.com/courses/try-git
+Immediate  accommodation available for 1or 2 guys in a 2BHK house in Daga nagar.
 
-http://codepen.io/TheLarkInn/post/angularjs-directive-labs-allowpattern-directive-more-fun-with-keycodes
+Details: 
+
+•	2 Bedrooms, 1 spacious Hall, 1 kitchen.
+•	24 Hour water supply.
+•	TV + Cable connection available.
+•	Bike parking available.
+•	Individual EB meter and individual water tank.
+•	500m from M-City Zero point.
+•	Grocery shop nearby to the house.
+•	Friendly and Peaceful locality.
+•	All the kitchen needs.
 
 
+Rent:  8000/3 or 10000/4 + 200 maintenance (Currently we are 2)
+Electricity bill once in every two months to be shared by all equally
+
+Note: Telugu Guys are preferred.
+
+For more details, Please ping me or call me:
+                       
+Hemanth : 8939469900
+
+Regards,
+Hemanth
+
+---------------
+
+Hi All,
+
+Immediate Accommodation available for 1 boy in 1BHK at Singaperumal koil
+                                                                                                                    
+Flat Description & Facilities:
+
+  Flat is located at Ground floor
+  Spacious hall, 1 bedroom.
+    TV 
+  Washing Machine
+  24/7 water supply 
+ 5 Mins walk for SP koil Railway station 
+ 5 Mins walk for Bus stop 
+  Super Markets and vegetables shops are at walkable distance
+
+
+Financials:
+
+  Rent : 2000 RS
+ Advance : 6667 RS
+  Other Expenses [Electricity, Water can]: As per usage and divided equally.
+
+    
+Contacts:
+Thirumala     - 9500402406
+Seenivasan  - 9788929360
+
+
+Thanks and Regards,
+Seenivasan B
+Senior Systems Engineer|Insurance, Cards and Payments| +91-9788929360| +91-44-37468513
+ 
+
+---------------
+
+
+Hi All,
+
+Immediate accommodation available for 1 GUY in a Fully furnished 2BHK at SP Kovil                                                                                                                             
+
+Flat Description & Facilities:
+
+	House is located at Ground floor
+	Spacious hall, kitchen, 2 bedrooms 
+	 Room furnished with AC
+	Fully equipped kitchen with Gas Set up
+	All kitchen utensils are available 
+	TV with Airtel DTH  connection
+	Refrigerator , Washing machine and geyser available 
+	Bus stop within 5-8 min of walk
+
+Financials:
+
+	Rent :12000/4
+	Advance : 5000
+
+    
+Contacts:
+
+Ragavan: 8056492293
+Pradeep: 9940145916
+
+Ragavan_Subburaj@infosys.com 
+Pradeep_Srinivasan@infosys.com
+Chitrakannan_B@infosys.com
+
+
+
+
+
+
+----------------------
+
+Hi All,
+ Immediate Accommodation available for 2 Guys in 2BHK flat at AAKAM TOWNSHIP, Hygrevar Enclave.
+Facilities:
+1.       Gated community with 24x7 security
+2.       Separate Parking area for every block
+3.       Spacious hall with well-ventilated rooms.
+4.       Washing machine & refrigerator .
+5.       Geysers installed in one washroom.                                               
+6.       Separate AC’s in both the rooms.
+7.    Maid available
+8.   15 min’s walk to Mcity Main Gate. (Akkam buses will be available).
+9.   Maintenance cost is included in rent
+10.   Electricity bill comes once in 2 month & shared between all
+11.   Broadband connection with WIFI available. 
+12. swimming pool and gym available in the apartment.
+13.   Hotels(Askoka Restaurant), Supermarkets(Hygrevar Super Marker) and other basic amenities are available nearby.
+
+Rent :  3500 Rs per head (including Maintenance, Net)
+Deposit Amount : 6000 Rs per head (Fully Refundable  )
+
+For further details please contact:
+
+Hanumtha : 9884252509
+Rafi : 9962077807
+Ganesh : 9585875253
+
+Thanks & Regards
+Hanumthasairam
+9884252509
+
+------------
+
+
+Hi BB,
+
+Immediate Accommodation available for 1 Guy in 2BHK flat in Smart Hillsdale Apartments – Chettipunniyam
+
+Rent Details: 
+ 
+Rent (including Maintenance) = 17100/6 = 2850
+
+	Electricity bill in 2 months shared by all.
+	Advance = 7,000 (Refundable Security Amount)
+
+
+Flat Details:
+
+	Well ventilated flat with balcony and spacious rooms.
+	Provided with TV (TataSky DTH connection), Refrigerator.
+	1 common spacious Hall.
+	Rail wire Wi-Fi connection available.
+	Kitchen is semi modular furnished with Induction cooker and other cooking appliances and utensils.
+	Washing machine is available.
+
+Room Details:
+
+	Split AC available in each room.
+	Attached washroom available for both the rooms.
+	Maid service available daily.
+
+Other Facilities: 
+                  
+	Shuttle facility from the apartment to Infosys gate. 
+	Gym and recreation facilities like TT, Carom and more available in the apartment.
+	Power Backup (24 x 7 hours).
+	Water Supply (24 x7 hours).
+	Plumber and Electrician are available round the clock
+	Lift Facility available.
+	Covered and Secure Parking Facility.
+	24 x 7 security with centralized surveillance camera.
+	Hygrevar Market nearby.
+	Door delivery is available for food.
+
+
+Contacts:
+
+For any further information you may contact any one of us–
+
+Vinod Kumar – 9866877688
+Sagar -  9676015531
+Deepak -  8056042393
+
+Regards,
+Sagar
+
+
+---------
+
+Accommodation is available for 1 Guy in a 1BHK house inside Mcity.
+
+Monthly Rent: 2000 per head(3 Guys can stay)
+
+Refundable Advance: 6500/-
+
+Facilities:
+-	24 hours water supply
+-	TV with DTH connection
+-	Walkable distance between Office and home.(10 Minutes)
+
+Electricity(not commercial) / Cable connection / Water Cans will be shared equally as per usage.
+
+Tamil Guys are preferred.
+
+If Interested please ping me or contact @ 9487831565
+
+Thanks & Regards,
+WESLEY
+
+----------
+
+Hi Friends,
+ 
+We are searching for 1guy available immediately .
+Below are the flat details :
+2BHK , First Floor 
+-          2 bedroom + Hall + Kitchen +  Balcony(Rooms are very spacious and very neatly maintained rooms)
+-           Washing machine is available 
+-           Induction stove is available cooking can be done in flat no restrictions  
+-          24x7 Hrs Power backup Inverter available.
+-          Very good ventilation in all rooms 
+-              Daily cleaning maid and flat care taker is available 
+-              Separate Spacious Car Parking and bike parking
+-              Separate 3 phase EB Connection and tenant needs to pay as per their meter charges.
+-              Fans & CFL fitted in all the rooms.
+-              24x7 water supply , electricity and security.
+-              Very near to Hygrevar super market 
+-              Vitrified tile flooring
+-              Both bedrooms have attached bathroom.
+-              Wifi available
+ 
+ 
+Rent + Maintenance=8750/4=2000 per each person
+EB= Depends on the usage 
+Advance: 8000 only
+ 
+If Interested please call us to the below numbers
+ 
+Ashok-9626839967
+Prasanth-9751859382
+ 
+LandMark: Beside hygrevar super market ,opp to Mahindra world city,less than 5mins walk from Mahindra city gate,chettipuniyam.
+ 
+ 
+Thanks and Regards,
+Ashok Kumar Penumudi
+Systems Engineer 
+Mobile : +91 9626839967
+Infosys® | Building Tomorrow’s Enterprise 
+Upcoming Holiday :- 15th April
